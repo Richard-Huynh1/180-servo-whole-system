fda5b2e0f39d3b9c17aaad6624421f8f14aaf7a5
Richard Huynh <emailofrichardh@gmail.com>
2026-06-03 08:46:46 -0400

    Add .gitignore

--- MainCode/secrets.h
+++ MainCode/secrets.h
@@ -1,7 +1,7 @@
 #ifndef SECRETS_H
 #define SECRETS_H
 
-const char* ssid = "Whole System";
+const char* ssid = "RC Car";
 const char* password = "12345678";  // Password needs to be at least 8 characters
 
 #endif // SECRETS_H
